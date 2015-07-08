05ebe9ff6380c1382e5de5700c86aa11ebc70235
juandeyby <juandeyby@localhost>
2015-07-08 01:48:09 -0500

    slider

--- war/js/principal.js
+++ war/js/principal.js
@@ -3,29 +3,38 @@ $(document).ready(function(){
 	function slider(){
 		switch (i % 3) {
 		case 1:
-			$("#slider").html('<img class="cambio" src="img/diversion.png">');
+			$("#slider").html('<img class="cambio" src="img/slider1.png">');
 			break;
 		case 2:
-			$("#slider").html('<img class="cambio" src="img/contactanos.png">');
+			$("#slider").html('<img class="cambio" src="img/slider2.png">');
 			break;
 		default:
-			$("#slider").html('<img class="cambio" src="img/comunicado.png">');
+			$("#slider").html('<img class="cambio" src="img/slider3.png">');
 			break;
 		}
 		i++;
 	}
 	setInterval(function(){slider()}, 4000);
-	$("#alumno").click(function(){
-		$("#formIngreso").css("background-image", 'url("../img/formAlumno.png")');
-		$("#formIngreso").load("/formAlumno.do")
+	
+	// Attach a submit handler to the form
+	$( "#searchForm" ).submit(function( event ) {
+	 
+	  // Stop form from submitting normally
+	  event.preventDefault();
+	 
+	  // Get some values from elements on the page:
+	  var $form = $( this ),
+	    term = $form.find( "input[name='s']" ).val(),
+	    url = $form.attr( "action" );
+	 
+	  // Send the data using post
+	  var posting = $.post( url, { s: term } );
+	 
+	  // Put the results in a div
+	  posting.done(function( data ) {
+	    var content = $( data ).find( "#content" );
+	    $( "#result" ).empty().append( content );
+	  });
 	});
-	$("#docente").click(function(){
-		$("#formIngreso").css("background-image", 'url("../img/formDocente.png")');
-		$("#formIngreso").load("/formDocente.do")
-	});
-	$("#apoderado").click(function(){
-		$("#formIngreso").css("background-image", 'url("../img/formApoderado.png")');
-		$("#formIngreso").load("/formApoderado.do")
-	});
-
+	
 });
