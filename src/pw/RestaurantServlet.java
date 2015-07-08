73d4afea0165852c27edf3f5155aa2d4a215bbd8
juanmanuelllanobarsaya <juanmanuelllanobarsaya@192.168.1.41>
2015-07-08 01:13:40 -0500

    servletcontrolador

---
+++ src/pw/RestaurantServlet.java
@@ -0,0 +1,98 @@
+package pw;
+
+import java.util.*;
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class RestaurantServlet extends HttpServlet {
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException, ServletException {
+		RequestDispatcher mandar = null ;
+			
+			String rest=req.getParameter("rest");
+			switch (rest){
+			case "INICIO":
+				mandar=getServletContext().getRequestDispatcher("/index.html");
+				break;
+			
+			case "Ubiquenos":
+				mandar=getServletContext().getRequestDispatcher("/WEB-INF/ubicacion.jsp");
+				break;
+				
+			case "Contactenos":
+				mandar=getServletContext().getRequestDispatcher("/WEB-INF/contactenos.jsp");
+				break;
+				
+			case "Reseña":
+				mandar=getServletContext().getRequestDispatcher("");
+				break;
+				
+			case "MENUS":
+				mandar=getServletContext().getRequestDispatcher("");
+				break;
+				
+			case "Entradas":
+				mandar=getServletContext().getRequestDispatcher("/WEB-INF/entradas.jsp");
+				break;
+				
+			case "Sopas":
+				mandar=getServletContext().getRequestDispatcher("/WEB-INF/sopa.jsp");
+				break;
+				
+			case "Ensaladas":
+				mandar=getServletContext().getRequestDispatcher("");
+				break;
+				
+			case "Plato De Fondo":
+				mandar=getServletContext().getRequestDispatcher("/WEB-INF/platosfondo.jsp");
+				break;
+				
+			case "Extras":
+				mandar=getServletContext().getRequestDispatcher("");
+				break;
+				
+			case "BEBIDAS":
+				mandar=getServletContext().getRequestDispatcher("");
+				break;
+				
+			case "Bebidas Alcoholicas":
+				mandar=getServletContext().getRequestDispatcher("");
+				break;
+				
+			case "Bibidas Gasificadas":
+				mandar=getServletContext().getRequestDispatcher("/WEB-INF/bebidasgas.jsp");
+				break;
+				
+			case "RESERVAR":
+				mandar=getServletContext().getRequestDispatcher("");	
+				break;
+				
+			case "Reservar Mesa":
+				mandar=getServletContext().getRequestDispatcher("/WEB-INF/reservaMesa.jsp");
+				break;
+				
+			case "Reservar Local":
+				mandar=getServletContext().getRequestDispatcher("");
+				break;
+			
+			case "LOGIN":
+				mandar=getServletContext().getRequestDispatcher("/WEB-INF/login.jsp");	
+				break;
+				
+			case "Registrarse":
+				mandar=getServletContext().getRequestDispatcher("/WEB-INF/formCliente.jsp");
+				break;
+				
+			default :
+			 	mandar=getServletContext().getRequestDispatcher("/index.html");	
+			break;
+			}
+			mandar.forward(req, resp);
+			
+		
+	}
+}
---
+++ war/WEB-INF/jsp/registro.jsp
@@ -0,0 +1,52 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/slider.js"></script>
+<link href="css/inicio.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Restaurante Lonccos</title>
+</head>
+<body>
+<section id="cero">
+		<div id="uno">
+			<div id="table">
+				<h2 align="center">Registro</h2>
+				
+				<form action="/saveCliente" method="post" onsubmit="return validar();">
+				<div class="row">
+				<div class="cell">Ingrese su Nombre :</div>
+				<div class="cell"><input type="text" name="nombre" size="40" pattern="([a-z]{2,20})|([a-z]{2,20} [a-z]{2,20})|([a-z]{2,20} [a-z]{2,20} [a-z]{2,20})" required></div>	
+				</div>
+				<div class="row">
+				<div class="cell">Ingrese su Apellido Paterno :</div>
+				<div class="cell"><input type="text" name="paterno" size="40" pattern="([a-z]{2,20})"  required></div>
+				</div>
+				<div class="row">
+				<div class="cell"> Ingrese su Apellido Materno :</div>
+				<div class="cell"><input type="text" name="materno" size="40" pattern="([a-z]{2,20})" required></div>	
+				</div>
+				<div class="row">
+				<div class="cell">DNI</div>
+				<div class="cell"><input type="text" name="dni" size="10" maxlength="8"  pattern="[0-9]{8}"required/></div>
+				</div>
+				<div class="row">
+				<div class="cell">Correo electronico</div> 
+				<div class="cell"><input type="text" name="email" size="40"  pattern="^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$" required/></div>	
+				</div>
+				<div class="row">
+				<div class="cell">Ingrese password </div>
+				<div class="cell"><input type="password" name="pass" size="40" placeholder="pass" required/></div>
+				</div>
+				<div class="row">
+				<div class="cell"></div>
+				<div class="cell"><input type="submit" value="registrarse"/></div>
+				</div>
+				</form>
+			</div>
+		</div>
+	</section>
+</body>
+</html>
